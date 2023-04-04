61d6182ab94dfe9ccd06973b867a014b598681bc
catalinfl <catalinfl02@gmail.com>
2023-04-04 10:17:49 +0300

    ui

--- client/src/components/SearchInput/Search.tsx
+++ client/src/components/SearchInput/Search.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Search.scss'
 import { AiOutlineSearch } from 'react-icons/ai'
+import { CiLocationOn } from 'react-icons/ci'
 
 const Search = () => {
   return (
@@ -8,13 +9,13 @@ const Search = () => {
         <div className="search__container">
             <div className="search__input">
                 <AiOutlineSearch className="search__inputIcon" />
-                <input className="search__inputField" type="text" placeholder="Caută anunțuri" />
+                <input className="search__inputField" type="text" placeholder="Caută anunțuri" /> 
             </div>
                 <div className="search__button">
-                    <button className="search__buttonField"> Caută acum </button>
+                    <button className="search__buttonField"> <span className="iconLocation"> <CiLocationOn/> </span> Toată România </button>
                 </div>
                 <div className="search__buttonCounty">
-                    <button className="search__buttonField"> Toata Romania </button>
+                    <button className="search__buttonField"> Caută acum <span className="iconSearch2"> <AiOutlineSearch /> </span> </button>
             </div>
         </div>
     </div>
